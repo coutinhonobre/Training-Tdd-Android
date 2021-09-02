c0415c6b0b34c6140fc8ace8c2f2128285d1a75b
coutinhoigor <81998304+coutinhoigor@users.noreply.github.com>
2021-09-02 02:56:51 -0300

    first test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Training-Tdd-Android
+
+<h1>Commands</h1>
+<ul>
+    <li><code>./gradlew test</code></li>
+</ul>
---
+++ chapter5/chapter5/app/src/test/java/com/raywenderlich/android/cocktails/game/model/GameUnitTests.kt
@@ -0,0 +1,22 @@
+package com.raywenderlich.android.cocktails.game.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GameUnitTests {
+    // 1
+    @Test
+    fun whenIncrementingScore_shouldIncrementCurrentScore() {
+        // 2
+        val game = Game()
+
+        // 3
+        game.incrementScore()
+
+        // 4
+        assertEquals(1, game.currentScore)
+        assertEquals("Current score should have been 1",
+            1, game.currentScore)
+
+    }
+}
